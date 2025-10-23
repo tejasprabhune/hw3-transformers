8f9e4428821e07738cbd165ebbc1b2f84c851c0f
Tejas Prabhune <tejas.prabhune@gmail.com>
2025-10-23 09:49:15 -0700

    coalesce data

--- seq2seq/data/screenplay.py
+++ seq2seq/data/screenplay.py
@@ -30,12 +30,19 @@ class ScreenplayDataset(Dataset):
                     else:
                         current_paragraph.append(line)
                 self.paragraphs[-1] = self.paragraphs[-1] + tokenizer.eos_token
+            if verbose:
+                print(path, num_para)
 
         while "\n" in self.paragraphs:
             self.paragraphs.remove("\n")
 
-        if verbose:
-            print(path, num_para)
+        original_len = len(self.paragraphs)
+
+        while len(self.paragraphs) > 0.1 * original_len:
+            r_idx = torch.randint(0, len(self.paragraphs) - 1, (1,))[0].item()
+            self.paragraphs[r_idx : r_idx + 2] = [
+                "".join(self.paragraphs[r_idx : r_idx + 2])
+            ]
 
     def __len__(self):
         return len(self.paragraphs)
@@ -50,3 +57,14 @@ class ScreenplayDataset(Dataset):
 def collate_fn(batch):
     pad_in = pad_sequence(batch, batch_first=True, padding_value=tokenizer.pad_token_id)
     return pad_in
+
+
+if __name__ == "__main__":
+    data = ScreenplayDataset(Path("data/lm/"), verbose=True)
+
+    idx = torch.randint(0, len(data), (10,)).tolist()
+
+    for i in idx:
+        para = data[i]
+        print(tokenizer.decode(para))
+        print("-" * 80)
